48d85c3b37e3af13fefdf9fd5680f2bd20568ec9
yrhiba <yahyarhiba@gmail.com>
2023-10-17 19:54:48 +0100

    new file:   Usaco/Problem_2_Speeding_Ticket.cpp

---
+++ Usaco/Problem_2_Speeding_Ticket.cpp
@@ -0,0 +1,87 @@
+/*
+Bronze
+Speeding Ticket
+Easy
+
+link to the problem:
+http://www.usaco.org/index.php?page=viewproblem2&cpid=568
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n, m; cin >> n >> m;
+
+	ll road_speeds[100], curr_p = 0, curr_c = 0;
+
+	for (ll i = 0; i < n; i++)
+	{
+		ll seg, speed;
+		cin >> seg >> speed;
+
+		for (ll j = 0; j < seg; j++)
+			road_speeds[curr_p++] = speed;
+	}
+
+	ll ans = 0;
+
+	for (ll i = 0; i < m; i++)
+	{
+		ll seg, speed;
+		cin >> seg >> speed;
+
+		for (ll j = 0; j < seg; j++)
+			ans = max(ans, speed - road_speeds[curr_c++]);
+	}
+
+	cout << ans << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	// comment this line for localy tests
+	setIO("speeding");
+	solve();
+}
