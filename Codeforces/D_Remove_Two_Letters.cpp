f8063dba8a00006c2a5a82b8bd1dd6add4fe0620
yrhiba <yahyarhiba@gmail.com>
2023-10-28 02:40:50 +0100

    new file:   Codeforces/D_Remove_Two_Letters.cpp

---
+++ Codeforces/D_Remove_Two_Letters.cpp
@@ -0,0 +1,78 @@
+/*
+link to the problem:
+https://codeforces.com/contest/1800/problem/D
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n; cin >> n;
+	string s; cin >> s;
+
+	ll res = 0;
+
+	for (ll i = 0; i < n; i++)
+	{
+		if (i == 0)
+			res += 1;
+		else
+		{
+			if ((i+1 < n) && s[i+1] != s[i-1])
+			{
+				res += 1;
+				continue;
+			}
+		}
+	}
+
+	cout << res << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
