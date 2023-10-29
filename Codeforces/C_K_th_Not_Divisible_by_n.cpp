fd587aa75407a56e1e2951c82ef997789744d2f9
yrhiba <yahyarhiba@gmail.com>
2023-10-29 15:35:56 +0100

    new file:   Codeforces/C_K_th_Not_Divisible_by_n.cpp

---
+++ Codeforces/C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,78 @@
+/*
+link to the problem:
+https://codeforces.com/contest/1352/problem/C
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+ll range_module(ll first, ll last, ll m)
+{
+	return ((last/m) - ((first-1)/m));
+}
+
+void solve()
+{
+	ll n, k; cin >> n >> k;
+
+	ll res = k;
+
+	ll start = 1, end = res;
+
+	ll x = range_module(start, end, n);
+	while (x > 0)
+	{
+		res += x;
+		start = end+1;
+		end = res;
+		x = range_module(start, end, n);
+	}
+	cout << res << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
