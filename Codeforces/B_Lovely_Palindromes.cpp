bfa7036564ca1a136d964afa6c4bbac51b376f14
yrhiba <yahyarhiba@gmail.com>
2023-11-01 21:24:43 +0100

    new file:   Codeforces/B_Lovely_Palindromes.cpp

---
+++ Codeforces/B_Lovely_Palindromes.cpp
@@ -0,0 +1,60 @@
+/*
+link to the problem:
+https://codeforces.com/contest/688/problem/B
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	string s; cin >> s;
+	string r = s;
+	reverse(r.begin(), r.end());
+	cout << s << r << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	solve();
+}
