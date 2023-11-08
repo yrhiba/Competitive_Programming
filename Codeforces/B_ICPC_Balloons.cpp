f6a9752504b08325b21f9bc6690c53f778dd24e3
yrhiba <yahyarhiba@gmail.com>
2023-11-08 22:22:06 +0100

    new file:   Codeforces/B_ICPC_Balloons.cpp

---
+++ Codeforces/B_ICPC_Balloons.cpp
@@ -0,0 +1,77 @@
+/*
+B. ICPC Balloons
+
+link to problem:
+https://codeforces.com/contest/1703/problem/B
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	vector<ll> ans(256, 0);
+
+	ll n; cin >> n;
+	string s; cin >> s;
+
+	for (char c : s)
+		ans[c] += 1;
+
+	ll res = 0;
+
+	for (char i = 'A'; i <= 'Z'; i++)
+	{
+		if (ans[i] > 0)
+			res += ans[i] + 1;
+	}
+
+	cout << res << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
