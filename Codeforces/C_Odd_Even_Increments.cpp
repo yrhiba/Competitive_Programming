642d75a00b1bf04be01f5c700a60e7ae4042fae4
yrhiba <yahyarhiba@gmail.com>
2023-11-10 19:26:11 +0100

    new file:   Codeforces/C_Odd_Even_Increments.cpp

---
+++ Codeforces/C_Odd_Even_Increments.cpp
@@ -0,0 +1,79 @@
+/*
+C. Odd/Even Increments
+
+link to the problem:
+https://codeforces.com/contest/1669/problem/C
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n; cin >> n;
+	vector<ll> a(n);
+
+	set<ll> odd, eve;
+
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+
+		if (i%2)
+			odd.insert(a[i]%2);
+		else
+			eve.insert(a[i]%2);
+	}
+
+	if (odd.size() == 1 && eve.size() == 1)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
