4784b747f221e88d86ae7d4f328320b8038294f4
yrhiba <yahyarhiba@gmail.com>
2023-11-17 20:06:37 +0100

    new file:   Codeforces/A_Game_with_Integers.cpp

---
+++ Codeforces/A_Game_with_Integers.cpp
@@ -0,0 +1,72 @@
+/*
+
+Codeforces Round 909 (Div. 3)
+
+A. Game with Integers
+
+link to the problem:
+https://codeforces.com/contest/1899/problem/A
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n; cin >> n;
+
+	if (((n + 1) % 3) == 0 || ((n - 1) % 3) == 0)
+	{
+		cout << "First\n";
+		return ;
+	}
+	cout << "Second\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
