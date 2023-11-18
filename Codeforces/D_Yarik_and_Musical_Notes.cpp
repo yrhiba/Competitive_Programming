285be7c9c26bdb6710caec11fb4bbab51859adb3
yrhiba <yahyarhiba@gmail.com>
2023-11-18 11:14:24 +0100

    new file:   Codeforces/D_Yarik_and_Musical_Notes.cpp

---
+++ Codeforces/D_Yarik_and_Musical_Notes.cpp
@@ -0,0 +1,81 @@
+/*
+
+Codeforces Round 909 (Div. 3)
+
+D. Yarik and Musical Notes
+
+link to the problem:
+https://codeforces.com/contest/1899/problem/D
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n; cin >> n;
+
+	map<ll, ll> mp;
+
+	for (ll i = 0; i < n; i++)
+	{
+		ll t; cin >> t;
+
+		mp[(t != 1) ? t : 2] += 1;
+	}
+
+	ll ans = 0;
+
+	for (auto &[i, j] : mp)
+		ans += (j * (j - 1)) / 2;
+
+	cout << ans << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
