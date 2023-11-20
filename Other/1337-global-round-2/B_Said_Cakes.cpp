59bec858b724e763195b52fcee4fe1e685111eb9
yrhiba <yahyarhiba@gmail.com>
2023-11-20 09:33:08 +0100

    new file:   Other/1337-global-round-2/B_Said_Cakes.cpp

---
+++ Other/1337-global-round-2/B_Said_Cakes.cpp
@@ -0,0 +1,77 @@
+/*
+
+1337 Competitive Programming Club - 1337 Global Round 2
+
+B. Said && Cakes
+
+link to the problem:
+find problems.pdf
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll n; cin >> n;
+
+	vector<ll> a(n);
+	for (ll &i : a)
+		cin >> i;
+
+	ll mine = *min_element(a.begin(), a.end());
+
+	ll ans = 0;
+	for (ll i = 0; i < n; i++)
+		ans += a[i] - mine;
+
+	cout << ans << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
