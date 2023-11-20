a03403f82c6be54bc0e22b4e67a02880c956c86f
yrhiba <yahyarhiba@gmail.com>
2023-11-20 10:23:35 +0100

    new file:   Other/1337-global-round-2/Hintes.cpp

---
+++ Other/1337-global-round-2/Hintes.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+
+# define ull unsigned long long
+# define ll long long
+
+using namespace std;
+
+
+void	hint_solving_a1()
+{
+	cout << "odd even odd even? is that true.." << endl;
+}
+
+void	hint_solving_a2()
+{
+	cout << "There is nothing gonna help you..." << endl;
+	cout << "you need to understand what your boss want? and do exactly what the say.";
+}
+
+void	hint_solving_b()
+{
+	cout << "Mazouz can't add cakes because is noob at cooking.." << ", ";
+	cout << "so he decide to look for a wife that she Know how to cook." << endl;
+
+	cout << "But till there now he can only decrement the value of cakes in each box...\n";
+}
+
+void	hint_solving_c()
+{
+	cout << "What What What..\n";
+	cout << "Try Visualize and out the problem into reallity..\n";
+	cout << "How you gonna solve this game if you just gonna use your hand..\n";
+	cout << "simulate the process in code...code like you think" << endl;
+}
+
+void	hint_solving_c()
+{
+	cout << "Imagine you have books each of them have different category...";
+	cout << "How you gonna now if there is some books with the same category.";
+	cout << "You don't really need Map.. if you know already that almost you,";
+	cout << " have only 7 category of books." << endl;
+	/* maybe you gonna look for seven boxes and for each book(category) you gonna put it 
+		in one off the boxes.
+		and at the end you gonna count how maney you have books for each category.
+	*/
+}
+
+void solve()
+{
+	//hint_solving_a1();
+	//hint_solving_a2();
+	//hint_solving_b();
+	//hint_solving_c();
+	//hint_solving_d();
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	solve();
+}
