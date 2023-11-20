9f862211e5722febf0e3e7a2c87e9fdeed2240e5
yrhiba <yahyarhiba@gmail.com>
2023-11-20 09:32:06 +0100

    new file:   Other/1337-global-round-2/A_2_Welcome_new_soldiers.cpp

---
+++ Other/1337-global-round-2/A_2_Welcome_new_soldiers.cpp
@@ -0,0 +1,78 @@
+/*
+
+1337 Competitive Programming Club - 1337 Global Round 2
+
+A2. Welcome new soldiers
+
+link to the problem:
+find problems.pdf
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+ll power(ll x, ll p)
+{
+	if (p == 0) return (1);
+	ll res = power(x, p/2);
+	if (p%2)
+		return ((res * res) * x);
+	return (res * res);
+}
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll x; cin >> x;
+
+	cout << "Division ";
+	if (x < 1400)
+		cout << 4;
+	else
+	if (x < 1600)
+		cout << 3;
+	else
+	if (x < 1900)
+		cout << 2;
+	else
+		cout << 1;
+	cout << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
