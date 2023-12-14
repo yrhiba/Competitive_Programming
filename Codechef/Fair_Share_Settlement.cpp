2c3bf402115dd835091ef5a632c5af9e8a50f84c
yrhiba <yahyarhiba@gmail.com>
2023-12-14 10:38:52 +0100

    new file:   Codechef/Fair_Share_Settlement.cpp

---
+++ Codechef/Fair_Share_Settlement.cpp
@@ -0,0 +1,48 @@
+/*
+
+Starters 112 Division 3 (Rated)
+
+Fair Share Settlement
+
+link to the problem:
+https://www.codechef.com/problems/FAIRSHARE
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+
+void solve()
+{
+	ll n, k; cin >> n >> k;
+
+	ll x = n / (k + 1);
+
+	cout << n - (x*k) << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
