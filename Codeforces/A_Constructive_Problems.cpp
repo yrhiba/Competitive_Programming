bf5d685c78147c2f2cf9b4d5bf9ca8cc1c6320fb
yrhiba <yahyarhiba@gmail.com>
2023-12-18 10:45:29 +0100

    new file:   Codeforces/A_Constructive_Problems.cpp

---
+++ Codeforces/A_Constructive_Problems.cpp
@@ -0,0 +1,45 @@
+/*
+
+Codeforces Round 915 (Div. 2)
+
+A. Constructive Problems
+
+link to the problem:
+https://codeforces.com/contest/1905/problem/A
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+
+void solve()
+{
+	ll n, m; cin >> n >> m;
+	cout << max(n, m) << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
