095262b159f956ef8dba5b979489955b22cd6e5c
yrhiba <yahyarhiba@gmail.com>
2024-01-08 11:21:52 +0100

    new file:   Codeforces/B_Two_Divisors.cpp

---
+++ Codeforces/B_Two_Divisors.cpp
@@ -0,0 +1,57 @@
+/*
+
+Good Bye 2023
+
+B. Two Divisors
+
+link to the problem:
+https://codeforces.com/contest/1916/problem/B
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+# define ull unsigned long long
+# define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+	if (b == 0) return (a);
+	return (gcd(b, a%b));
+}
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a, b));
+}
+
+void solve()
+{
+	ll a, b; cin >> a >> b;
+	ll res = lcm(a, b);
+	if (res == b) res *= ((a==1) ? res : (b / a));
+	cout << res << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
