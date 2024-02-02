1abc408a7b8237a71cc8c69f68d20494630a4676
yrhiba <yahyarhiba@gmail.com>
2024-02-02 13:53:42 +0100

    modified:   Leetcode/212.WordSearchII.cpp

--- Leetcode/212.WordSearchII.cpp
+++ Leetcode/212.WordSearchII.cpp
@@ -5,7 +5,96 @@ link to the problem:
 https://leetcode.com/problems/word-search-ii/
 */
 
+/* optimized-Solution - ~610 ms */
+int dr[] = {1, -1, 0, 0};
+int dc[] = {0, 0, 1, -1};
+
+struct Node {
+	Node *links[26];
+	bool endOfWord;
+	Node()
+	{
+		for (int i = 0; i < 26; i++)
+			links[i] = nullptr;
+		endOfWord = false;
+	}
+};
+
+class Solution {
+private:
+	vector<string> ans;
+	string cur = "";
+	Node *root = new Node();
+	int n, m;
+
+	void insert(string &word)
+	{
+		Node *it = root;
+		for (char c : word)
+		{
+			if (it->links[c - 'a'] == nullptr)
+				it->links[c - 'a'] = new Node();
+			it = it->links[c - 'a'];
+		}
+		it->endOfWord = true;
+	}
+
+	void dfs(vector<vector<char>>& board, vector< vector<bool> > &vis, int i, int j, Node *it)
+	{
+		cur.push_back(board[i][j]);
+
+		vis[i][j] = true;
+
+		if (it->endOfWord)
+		{
+			it->endOfWord = false;
+			ans.push_back(cur);
+		}
+
+		for (int d = 0; d < 4; d++)
+		{
+			int ni = i + dr[d];
+			int nj = j + dc[d];
+
+			if (ni < 0 || nj < 0 || ni >= n || nj >= m)
+				continue;
+
+			if (vis[ni][nj])
+				continue;
+
+			if (it->links[board[ni][nj] - 'a'])
+				dfs(board, vis, ni, nj, it->links[board[ni][nj] - 'a']);
+		}
+
+		vis[i][j] = false;
+		cur.pop_back();
+	}
+
+public:
+	vector<string> findWords(vector<vector<char>>& board, vector<string>& words)
+	{
+		for (string &w : words)
+			insert(w);
+
+		n = board.size();
+		m = board[0].size();
+
+		vector< vector<bool> > vis(n, vector<bool>(m, false));
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+			{
+				if (root->links[board[i][j] - 'a'])
+					dfs(board, vis, i, j, root->links[board[i][j] - 'a']);
+			}
+
+		return (ans);
+	}
+};
+
+
 /* First Solution - ~2100 ms */
+/*
 int dr[] = {1, -1, 0, 0};
 int dc[] = {0, 0, 1, -1};
 
@@ -105,3 +194,4 @@ public:
 		return ans;
 	}
 };
+*/
