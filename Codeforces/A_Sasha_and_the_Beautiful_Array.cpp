d955c9a6f803f5c2039a6a65def0391a0dfd008b
yrhiba <yahyarhiba@gmail.com>
2024-02-16 00:58:49 +0100

    new file:   Codeforces/A_Sasha_and_the_Beautiful_Array.cpp

---
+++ Codeforces/A_Sasha_and_the_Beautiful_Array.cpp
@@ -0,0 +1,46 @@
+/*
+A. Sasha and the Beautiful Array
+
+link to the problem:
+https://codeforces.com/contest/1929/problem/A
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <numeric>
+# define ull unsigned long long
+# define ll long long
+const ll mod = 1e9 + 7;
+using namespace std;
+
+void solve()
+{
+	ll n; cin >> n;
+	vector<ll> a(n);
+	for (ll &i : a) cin >> i;
+	sort(a.begin(), a.end());
+	ll ans = 0;
+	for (ll i = 1; i < n; i++) ans += a[i] - a[i-1];
+	cout << ans << "\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
