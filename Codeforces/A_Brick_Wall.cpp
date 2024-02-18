34f2541d897daf196ed46e4ccb262be911bfd9eb
yrhiba <yahyarhiba@gmail.com>
2024-02-18 20:54:01 +0100

    new file:   Codeforces/A_Brick_Wall.cpp

---
+++ Codeforces/A_Brick_Wall.cpp
@@ -0,0 +1,44 @@
+/*
+A. Brick Wall
+
+link to the problem:
+https://codeforces.com/contest/1918/problem/A
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <numeric>
+# define ull unsigned long long
+# define ll long long
+const ll mod = 1e9 + 7;
+using namespace std;
+
+void solve()
+{
+	ll n, m; cin >> n >> m;
+
+	ll ans = (m/2) * n;
+
+	cout << ans << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t; cin >> t;
+	while (t--)
+		solve();
+}
