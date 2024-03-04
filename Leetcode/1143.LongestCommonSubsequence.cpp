3dd871ccf386f25269e25f3a43ec9674a8ef1ceb
yrhiba <yahyarhiba@gmail.com>
2024-03-04 19:33:18 +0100

    modified:   Leetcode/1143.LongestCommonSubsequence.cpp

--- Leetcode/1143.LongestCommonSubsequence.cpp
+++ Leetcode/1143.LongestCommonSubsequence.cpp
@@ -27,3 +27,33 @@ public:
 		return rec(text1, text2, 0, 0);
 	}
 };
+
+/*
+iterative solution O(n*m)
+
+class Solution
+{
+public:
+	int longestCommonSubsequence(string text1, string text2)
+	{
+		int n = text1.size();
+		int m = text2.size();
+		//
+		vector< vector<int> > dp(n+1, vector<int>(m+1, -1));
+		//
+		for (int i = n; i >= 0; i--)
+			for (int j = m; j >= 0; j--)
+			{
+				if (i == n || j == m) dp[i][j] = 0;
+				else
+				{
+					if (text1[i] == text2[j]) dp[i][j] = 1 + dp[i+1][j+1];
+					else dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+				}
+			}
+		//
+		return dp[0][0];
+	}
+};
+
+*/
