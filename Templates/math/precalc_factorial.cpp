786bcbe4d581e65cccf0a0ccf8942691d2aac9ce
yahya rhiba <76631430+yrhiba@users.noreply.github.com>
2024-09-21 14:28:22 +0100

    Update precalc_factorial.cpp

--- Templates/math/precalc_factorial.cpp
+++ Templates/math/precalc_factorial.cpp
@@ -1,17 +1,48 @@
 #include "header.hpp"
 
-#define FACT_MAXN 100001
-
-ll fact[FACT_MAXN];
-
-void precalc_factorial(void)
+/* Binominial
+Usage:
+	1- precumpute:
+		InverseofNumber(mod);
+		InverseofFactorial(mod);
+		factorial(mod);
+*/
+// array to store inverse of 1 to N
+ll factorialNumInverse[MAX_N + 1];
+// array to precompute inverse of 1! to N!
+ll naturalNumInverse[MAX_N + 1];
+// array to store factorial of first N numbers
+ll fact[MAX_N + 1];
+// Function to precompute inverse of numbers
+void InverseofNumber(ll p)
 {
-	/*factorial precalculation*/
+	naturalNumInverse[0] = naturalNumInverse[1] = 1;
+	for (int i = 2; i <= MAX_N; i++)
+		naturalNumInverse[i] = naturalNumInverse[p % i] * (p - p / i) % p;
+}
+// Function to precompute inverse of factorials
+void InverseofFactorial(ll p)
+{
+	factorialNumInverse[0] = factorialNumInverse[1] = 1;
+	// precompute inverse of natural numbers
+	for (int i = 2; i <= MAX_N; i++)
+		factorialNumInverse[i] = (naturalNumInverse[i] * factorialNumInverse[i - 1]) % p;
+}
+// Function to calculate factorial of 1 to N
+void factorial(ll p)
+{
 	fact[0] = 1;
-	fact[1] = 1;
-	for (ll i = 2; i <= FACT_MAXN; i++)
-	{
-		fact[i] = (i * fact[i-1]);
-		fact[i] %= mod;
+	// precompute factorials
+	for (int i = 1; i <= MAX_N; i++) {
+		fact[i] = (fact[i - 1] * i) % p;
 	}
+}
+// Function to return nCr % p in O(1) time
+ll Binomial(ll N, ll R, ll p)
+{
+	// n C r = n!*inverse(r!)*inverse((n-r)!)
+	ll ans = ((fact[N] * factorialNumInverse[R])
+				% p * factorialNumInverse[N - R])
+				% p;
+	return ans;
 }
