02a242c27929e6575e07b1b074aed7794aa1888b
yrhiba <yahyarhiba@gmail.com>
2024-11-26 21:35:00 +0100

    modified:   Templates/Strings/suffixArray.cpp
    upgrade time complexity from o(n log n log n) -> o (n log n)
    remove the sort n log n to radix sort.
    reduce the memory complexity to o(n).

--- Templates/Strings/suffixArray.cpp
+++ Templates/Strings/suffixArray.cpp
@@ -1,15 +1,26 @@
 #include "header.hpp"
 
-vector<int> suffixArray(string &s)
+/*
+	res = empty vector where the answer got stored
+*/
+void suffixArray(vector<int> &res, string &s)
 {
 	/*
 		suffix Array implementation;
-		algorithm idea - ressources: https://codeforces.com/edu/course/2/lesson/2/1
+		source algorithm idea:
+		https://codeforces.com/edu/course/2/lesson/2/1
 	*/
 
 	// special Cases no need to run the algorithm for them
-	if (s.empty()) return vector<int>{0};
-	if (s.size() == 1) return vector<int>{1, 0};
+	if (s.empty()) {
+		res = vector<int>{0};
+		return ;
+	}
+	//
+	if (s.size() == 1) {
+		res = vector<int>{1, 0};
+		return ;
+	}
 
 	// starting of the algorithms steps
 	s += '$';
@@ -17,7 +28,8 @@ vector<int> suffixArray(string &s)
 	int n = int(s.size()), k = 0;
 	for (; (1ll<<k) < n; k++);
 
-	vector<vector<int>> eqClass(n, vector<int>(k+1));
+	vector<int> eqClass(n);
+	res = vector<int>(n);
 
 	// first step k = 0
 	{
@@ -28,39 +40,45 @@ vector<int> suffixArray(string &s)
 		sort(t.begin(), t.end());
 		for (int i = 0, idx = -1; i < n; i++) {
 			if (!i || t[i].first != t[i-1].first) {
-				eqClass[t[i].second][0] = ++idx;
+				eqClass[t[i].second] = ++idx;
 			} else {
-				eqClass[t[i].second][0] = eqClass[t[i-1].second][0];
+				eqClass[t[i].second] = eqClass[t[i-1].second];
 			}
+			// initial suffix array result
+			res[i] = t[i].second;
 		}
 	}
 
 	vector<vector<int>> t(n);
+	vector<vector<int>> cnt(n);
 
 	// second step k:1 -> 2^k >= n
 	for (int i = 1; i <= k; i++) {
 		int incr = 1ll<<(i-1);
 		for (int j = 0; j < n; j++) {
-			t[j] = {eqClass[j][i-1], eqClass[(j+incr) % n][i-1], j};
+			cnt[eqClass[(res[j] - incr + n) % n]].push_back(res[j]);
 		}
-		sort(t.begin(), t.end());
+		for (int j = 0, idx = 0; j < n; j++) {
+			for (int res_j : cnt[j]) {
+				t[idx++] = {
+						j,
+						eqClass[res_j],
+						(res_j - incr + n) % n
+					};
+			}
+			cnt[j].clear();
+		}
 		for (int j = 0, idx = -1; j < n; j++) {
 			if (!j || t[j][0] != t[j-1][0] || t[j][1] != t[j-1][1]) {
-				eqClass[t[j][2]][i] = ++idx;
+				eqClass[t[j][2]] = ++idx;
 			} else {
-				eqClass[t[j][2]][i] = eqClass[t[j-1][2]][i];
+				eqClass[t[j][2]] = eqClass[t[j-1][2]];
 			}
+			// update the result suffix array
+			res[j] = t[j][2];
 		}
-	}
-
-	// 
-	vector<int> res(n);
-	for (int i = 0; i < n; i++) {
-		res[i] = t[i][2];
 	}
 
 	// remove the special added charachter
 	s.pop_back();
-
-	return (res);
 }
