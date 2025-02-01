ea9fc73281af3188fe0685c6ef75b28c86d42aec
yrhiba <yahyarhiba@gmail.com>
2025-02-01 08:25:46 +0100

    new file:   Content/SQL/Leetcode/SQL_50/595. Big Countries.sql

---
+++ Content/SQL/Leetcode/SQL_50/595. Big Countries.sql
@@ -0,0 +1,8 @@
+/*
+595. Big Countries
+
+Link: https://leetcode.com/problems/big-countries/
+*/
+
+-- PostgreSQL query statement
+SELECT name,area,population FROM World WHERE area>=3000000 OR population>=25000000;
