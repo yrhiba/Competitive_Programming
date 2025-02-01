70635a24b031d861ed3f6b74fc83ef473f96c5a0
yrhiba <yahyarhiba@gmail.com>
2025-02-01 08:22:34 +0100

    new file:   Content/SQL/Leetcode/SQL_50/1757. Recyclable and Low Fat Products.sql

---
+++ Content/SQL/Leetcode/SQL_50/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,8 @@
+/*
+1757. Recyclable and Low Fat Products
+
+Link: https://leetcode.com/problems/recyclable-and-low-fat-products
+*/
+
+-- PostgreSQL query statement
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable='Y';
